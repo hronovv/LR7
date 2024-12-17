60d3cd9e8506495bf7ed40eef864b0a8a889fe17
Ivan <ivanonlane@gmail.com>
2024-12-17 12:58:21 +0300

    1st task solved

---
+++ Task_1/Task1.cpp
@@ -0,0 +1,10 @@
+#include "/Users/personal/Documents/453504/ОАиП/LR7/source/header.h"
+#include <iostream>
+#include <string>
+int main() {
+    std::string str;
+    std::cout << "Введите число в виде прямого кода, учитывая следующий формат - 'знак числа(1 если -, 0 если +) + '0' + число в двоичном виде:\n";
+    std::cin >> str;
+    std::cout << "Исходное число " << str << " будет в дополнительном коде так:\n" << convert_to_complement(str) << std::endl;
+    return 0;
+}
