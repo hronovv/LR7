920aca08a30722724ba0b5a96a8afd42f0f0019f
Ivan <ivanonlane@gmail.com>
2024-12-19 07:14:39 +0300

    moved functions

--- Task_3/Task3.cpp
+++ Task_3/Task3.cpp
@@ -5,47 +5,6 @@
 typedef long long ll;
 const std::string ascii_end =  "\x1b[0m";
 const std::string ascii_green = "\x1b[0;32m";
-std::string add_in_base(const std::string& num1, const std::string& num2, short base) {
-    std::string result;
-    int carry = 0;
-    int maxLength = std::max(num1.size(), num2.size());
-    for (int i = 0; i < maxLength || carry; i++) {
-        int digit1 = (i < num1.size()) ? base_symbols.find(num1[num1.size() - 1 - i]) : 0;
-        int digit2 = (i < num2.size()) ? base_symbols.find(num2[num2.size() - 1 - i]) : 0;
-        int sum = digit1 + digit2 + carry;
-        carry = sum / base;
-        result += base_symbols[sum % base];
-    }
-    std::reverse(result.begin(), result.end());
-    return result;
-}
-std::string subtract_in_base(const std::string& num1, const std::string& num2, short base, ll number1, ll number2) {
-    if (number2 > number1) {
-        return "-" + subtract_in_base(num2, num1,base,number2,number1);
-    }
-    std::string result;
-    int difference;
-    int borrow = 0;
-    int maxLength = std::max(num1.size(), num2.size());
-    for (int i = 0; i < maxLength; i++) {
-        int digit1 = (i < num1.size()) ? base_symbols.find(num1[num1.size() - 1 - i]) : 0;
-        int digit2 = (i < num2.size()) ? base_symbols.find(num2[num2.size() - 1 - i]) : 0;
-        digit1 -= borrow;
-        if (digit1 < digit2) {
-            digit1 += base;
-            borrow = 1;
-        } else {
-            borrow = 0;
-        }
-        difference = digit1 - digit2;
-        result += base_symbols[difference];
-    }
-    while (result.size() > 1 && result.back() == base_symbols[0]) {
-        result.pop_back();
-    }
-    std::reverse(result.begin(), result.end());
-    return result;
-}
 int main() {
     std::string final;
     ll number1, number2;
